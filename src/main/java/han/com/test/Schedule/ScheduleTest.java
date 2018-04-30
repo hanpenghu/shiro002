c4e51d617a7eee1d79532ade92dba7e89cc110a7
hanpenghu <563909408@qq.com>
2018-04-30 23:14:38 +0800

    2018_4_30   weekday(1)   23:14:35

---
+++ src/main/java/han/com/test/Schedule/ScheduleTest.java
@@ -0,0 +1,14 @@
+package han.com.test.Schedule;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+//@Component
+//public class ScheduleTest {
+//
+//    //单位毫秒
+//    @Scheduled(fixedDelay = 1000,initialDelay = 10000)
+//    public static void f(){
+//
+//    }
+//
+//}
---
+++ src/main/java/han/com/test/service/impl/TestS1.java
@@ -0,0 +1,14 @@
+package han.com.test.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class TestS1 {
+
+    @Transactional
+    public void f(){
+
+    }
+
+}
